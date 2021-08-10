a6912082f94cbf325447fbab72f3e8b823345a91
corrosion <corrosion521@naver.com>
2021-08-10 13:39:55 +0900

    "LinkedList"
    기본개념,코드

---
+++ LinkedList.md
@@ -0,0 +1,2 @@
+1. element(노드)끼리연결
+![img_2.png](img_2.png)
---
+++ LinkedList.py
@@ -0,0 +1,23 @@
+'''
+출처: 인프런 강의
+
+지구에서 제일 쉽게 설명한 자료구조와 알고리즘- 개복치 개발자
+
+수업 때 들은 내용을 바탕으로 간략하게 요약한 내용입니다. 틀린 내용이 있거나 고쳐야 할 점이 있다면 가르쳐주신다면 감사하겠습니다.
+
+문제가 될 시 바로 게시글 내리도록 하겠습니다.
+
+https://www.inflearn.com/course/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-1/dashboard
+'''
+
+class Node :
+    def _init_(self,data):
+        self.data = data
+        self.next = None
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+
+node1.next = node2
+node2.next = node3
