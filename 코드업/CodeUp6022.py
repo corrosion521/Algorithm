6b43ba10d203ef0aafeb8929b7eefd8e7f721df2
정지오 <corrosion521@naver.com>
2022-01-20 14:22:04 +0900

    "코드업 6022 파이썬 수정"

--- 코드업/CodeUp6022.py
+++ 코드업/CodeUp6022.py
@@ -1,5 +1,6 @@
 '''
 print()에는 end='\n'가 디폴트라는 점 유의
+또 sep은 , 단위로 나눌 때(띄어쓰기) 사용가능함을 유의, +는 안됨.
 '''
 
 s = input()
@@ -7,4 +8,5 @@ s = input()
 for i in range(3):
     print(s[i*2]+s[i*2+1],end=' ')
 
+#print("1"+"2",sep='+')
 #print(s[0:2],s[2:4],s[4:6],sep=' ')도 있다.
