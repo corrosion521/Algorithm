730cf2dc87ab55c13f1a0d4af1ee656a97da2571
정지오 <corrosion521@naver.com>
2022-01-22 04:04:32 +0900

    "코드업 6074 파이썬"

--- 코드업/CodeUp6074.py
+++ 코드업/CodeUp6074.py
@@ -3,4 +3,18 @@ al = input()
 
 #출력 : a~입력까지의 알파벳 출력
 for i in range(ord('a'),ord(al)+1):
-    print(chr(i),end=' ',)
+    print(chr(i),end=' ')
+
+
+'''
+숫자 뿐 아니라
+문자도 대소비교 가능함(아스키 코드)
+
+'''
+'''
+c = input()
+k = 'a'
+while c>=k:
+    print(k,end=' ')
+    k=chr(ord(k)+1)
+'''
