cd869b247e1e121a3a8c51eaa229a62ee66f6702
정지오 <corrosion521@naver.com>
2022-01-22 13:16:23 +0900

    "코드업 6082 파이썬"

---
+++ 코드업/CodeUp6082.py
@@ -0,0 +1,23 @@
+'''
+문제조건 똑바로 읽기.
+기본이다.
+괜히 제대로 안읽으면 훨씬 어렵게 풀게됨.
+'''
+'''
+n = int(input())
+
+for i in range(1,n+1):
+        if (i%10)%3!=0:
+            print(i,end=' ')
+        elif i%10==0:
+            print(i,end=' ')
+        else:
+            print("X",end=' ')
+'''
+a = int(input())
+
+for i in range(1,a+1):
+    if(i%10==3 or i%10==6 or i%10==9):
+        print('X ',end='')
+    else:
+        print('%d '%i,end='')
