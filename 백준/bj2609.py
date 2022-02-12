72d2c9a1933da23809b33b4d0ce4989f18c1541d
정지오 <corrosion521@naver.com>
2022-02-12 19:09:26 +0900

    "백준 2609 파이썬 수정"

--- 백준/bj2609.py
+++ 백준/bj2609.py
@@ -4,24 +4,35 @@
 ex) gcd(24,18) = gcd(18,6) =gcd(6,0) . b가 0이되는 순간의 6이 최대공약수.
 2) 최대공배수 : A로 나눠도 ,B로 나눠도 떨어지는 수중에 가장 작은 수
 == A*B/gcd(a/b) -- (A*B안에 최대공약수가 2개(A에 하나 , B에 하나)들어가있음을 염두, 따라서 gcd로 나누는거 )
-2. 재귀함수: 호출 후 return시 불러낸 함수로 돌아간다는 점 명심할 것. 최초의 함수로 return하는게 아님!
+2. 재귀함수: 반환(return)에 유의할 것. return 있고 없고 차이가 크다.
 3. swap: ex) a,b= b,a 딱히 순서가 없음. 동시에 바꾼다고 생각하면 됨.
 '''
 
 import sys
-
+# 최대공약수
 def gcd(a,b):
     while b!=0:
         a,b= b,a%b
     return a
 
-def lcm(a,b):
-    return a*b/gcd(a,b)
+# 최대공약수 - 재귀
+def gcd_r(a,b):
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
 
+# 최소공배수
+def lcm(a,b):
+    return a*b//gcd(a,b)
+
+# 입력
 a,b = map(int, sys.stdin.readline().split())
 
+# 대,소 순으로 swap
 if a<b:
     a,b= b,a
 
-print(gcd(a,b))
-print(int(lcm(a,b)))
+#출력
+print(gcd_r(a,b))
+print(lcm(a,b))
