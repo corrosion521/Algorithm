d6076d5486c046ac3d9d495716b5e08f8f19405b
정지오 <corrosion521@naver.com>
2022-02-23 13:19:06 +0900

    "백준 4153 파이썬 수정"

--- 백준/bj4153.py
+++ 백준/bj4153.py
@@ -3,14 +3,16 @@
 => 예약어를 변수명으로 사용하면 이렇게 됨
 ex) max 를 변수명
 '''
-while True:
-    a = list(map(int,input().split()))
-    m = max(a)
-    a.remove(m)
+import sys
 
-    if a[0] ==0 and a[1]==0 and m==0:# 브레이크
-        break
-    if a[0]**2 + a[1]**2  == m**2: # 계산
-        print("right")
-    else:
-        print("wrong")
+while True:
+  l1 = (list(map(int,sys.stdin.readline().split())))
+  l1.sort()
+
+  if l1[0]==0 and l1[1]==0 and l1[2] == 0:
+    break
+
+  if l1[0]**2 + l1[1]**2 == l1[2]**2:
+    print("right")
+  else:
+    print("wrong")
